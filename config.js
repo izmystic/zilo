066a3e3fe872294675c0fb6d7243497b13692580
izmystic <mystic@izmystic.dev>
2023-10-07 20:02:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    OWNER_IDS: [""], // bot owner ids
-    SUPPORT_SERVER: "", // bot support server invite url
+    OWNER_IDS: ["680957940647329829"], // bot owner ids
+    SUPPORT_SERVER: "https://discord.gg/EsGUFcn7RP", // bot support server invite url
     PREFIX_COMMANDS: {
-        ENABLED: true, // enable/disable prefix commands
+        ENABLED: false, // enable/disable prefix commands
         DEFAULT_PREFIX: "!", // default prefix
     },
     INTERACTIONS: {
-        SLASH: false, // enable slash interactions
-        CONTEXT: false, // enable context interactions
-        GLOBAL: false, // enable global interactions
-        TEST_GUILD_ID: "xxxxxxxxxxx", // testing guild id (test commands here first)
+        SLASH: true, // enable slash interactions
+        CONTEXT: true, // enable context interactions
+        GLOBAL: true, // enable global interactions
+        TEST_GUILD_ID: "1157101410710736946", // testing guild id (test commands here first)
     },
     EMBED_COLORS: {
-        BOT_EMBED: "#068ADD", // general bot embed color
+        BOT_EMBED: "#c6a129", // general bot embed color
         TRANSPARENT: "#36393F", // invisible embed color
-        SUCCESS: "#00A56A", // success embed color
-        ERROR: "#D61A3C", // error embed color
-        WARNING: "#F7E919", // warning embed color
+        SUCCESS: "#37b24d", // success embed color
+        ERROR: "#f03e3e", // error embed color
+        WARNING: "#f59f00", // warning embed color
     },
     CACHE_SIZE: {
         GUILDS: 100,
@@ -30,13 +30,13 @@ module.exports = {
     // PLUGINS
 
     AUTOMOD: {
-        ENABLED: false, // enable/disable automod
+        ENABLED: true, // enable/disable automod
         LOG_EMBED: "#36393F", // log embed color
         DM_EMBED: "#36393F", // dm embed color
     },
 
     ECONOMY: {
-        ENABLED: false, // enable/disable economy
+        ENABLED: true, // enable/disable economy
         CURRENCY: "₪", // currency symbol
         DAILY_COINS: 100, // coins to be received by daily command
         MIN_BEG_AMOUNT: 100, // minimum coins to be received when beg command is used
@@ -44,16 +44,16 @@ module.exports = {
     },
 
     MUSIC: {
-        ENABLED: false, // enable/disable music system
+        ENABLED: true, // enable/disable music system
         IDLE_TIME: 60, // seconds before bot disconnects from an idle voice channel
         MAX_SEARCH_RESULTS: 5, // maximum search results
-        DEFAULT_SOURCE: "SC", // YT = youtube, YTM = youtube music, SC = sound cloud
+        DEFAULT_SOURCE: "YT", // YT = youtube, YTM = youtube music, SC = sound cloud
         // add lavalink nodes here, https://github.com/freyacodes/Lavalink
         LAVALINK_NODES: [
             {
-                host: "localhost",
+                host: "0.0.0.0",
                 port: 2333,
-                password: "youshallnotpass",
+                password: "poolman12",
                 id: "Local Node",
                 secure: false,
             },
@@ -68,16 +68,16 @@ module.exports = {
     },
 
     IMAGE: {
-        ENABLED: false, // enable/discord image manipulation
+        ENABLED: true, // enable/discord image manipulation
         BASE_API: "https://strangeapi.fun/api", // api link
     },
 
     INVITE: {
-        ENABLED: false, // enable/disable invite tracking
+        ENABLED: true, // enable/disable invite tracking
     },
 
     MODERATION: {
-        ENABLED: false, // enable/disable moderation
+        ENABLED: true, // enable/disable moderation
         EMBED_COLORS: {
             TIMEOUT: "#102027", // timeout embed color
             UNTIMEOUT: "#4B636E", // untimeout embed color
@@ -95,16 +95,16 @@ module.exports = {
     },
 
     PRESENCE: {
-        ENABLED: false, // enable/disable presence
+        ENABLED: true, // enable/disable presence
         STATUS: "online", // current status (online, idle, dnd, invisible)
         TYPE: "WATCHING", // status type (PLAYING, LISTENING, WATCHING, COMPETING)
         MESSAGE: "{members} members in {servers} servers", // status message ({members}, {servers})
     },
 
     STATS: {
-        ENABLED: false,
-        XP_COOLDOWN: 5, // Cooldown in seconds between messages
-        DEFAULT_LVL_UP_MSG: "{member:tag}, You just advanced to **Level {level}**",
+        ENABLED: true, // enable/disable stat tracking
+        XP_COOLDOWN: 5, // cooldown in seconds between messages
+        DEFAULT_LVL_UP_MSG: "{member:tag}, You just advanced to **Level {level}**", // level up message ({server}, {count}, {member:id}, {member:name}, {member:mention}, {member:tag}, {level})
     },
 
     SUGGESTIONS: {
@@ -113,14 +113,14 @@ module.exports = {
             UP_VOTE: "⬆️", // upvote emoji
             DOWN_VOTE: "⬇️", // downvote emoji
         },
-        DEFAULT_EMBED: "#4F545C",
-        APPROVED_EMBED: "#43B581",
-        DENIED_EMBED: "#F04747",
+        DEFAULT_EMBED: "#4F545C", // default suggestion embed
+        APPROVED_EMBED: "#43B581", // approved suggestion embed
+        DENIED_EMBED: "#F04747", // denied suggestion embed
     },
 
     TICKET: {
-        ENABLED: false, // enable/disable ticket system
-        CREATE_EMBED: "#068ADD",
-        CLOSE_EMBED: "#068ADD",
+        ENABLED: true, // enable/disable ticket system
+        CREATE_EMBED: "#068ADD", // create ticket embed color
+        CLOSE_EMBED: "#068ADD", // close ticket embed color
     },
 };
